9faca06473d3a2863a6f22446b7115486418e8c6
zezecoder <76428041+zezecoder@users.noreply.github.com>
2020-12-22 10:03:14 +0000

    Add files via upload

---
+++ 8.listening lists.py
@@ -0,0 +1,20 @@
+friends = ["Kevin", "Karen", "Jim"]
+print(friends)
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(str(friends[1:]) + "are cool people!")
+print(str(friends[2:]) + " is a cool person!")
+print(str(friends[0:]) + "are cool people!")
+lucky_numbers = [4,8,15,16,23,42]
+friends.append("Bill")
+print(friends)
+friends.insert(1, "👉")
+print(friends)
+friends.pop(1)
+print(lucky_numbers)
+lucky_numbers.reverse()
+print(lucky_numbers)
+friends.clear()
+
+
